a30b58e94cc5ba6a17a19bb3831712be65cb19f7
fredericoeca <frederico-cabral@hotmail.com>
2021-01-12 14:14:01 -0300

    first commit

---
+++ src/main/java/com/compasso/CompassoUolApplication.java
@@ -0,0 +1,13 @@
+package com.compasso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassoUolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompassoUolApplication.class, args);
+	}
+
+}
